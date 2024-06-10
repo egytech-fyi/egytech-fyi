cb974afeddf76c5e92052f5fe931bc65e7223121
Ahmed Gamal <163402660+ahmad-g-mustafa@users.noreply.github.com>
2024-06-10 23:14:14 +0300

    eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   resolve: {
@@ -20,4 +19,4 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-});
+})
