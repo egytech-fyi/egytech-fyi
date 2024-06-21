296bbd9bad9d0b0f374da4eda7026a418de4102c
BassemHalim <bassemamir459@gmail.com>
2024-06-21 03:53:32 -0700

    🔥 removed unsed code

--- src/services/getSurvey.ts
+++
@@ -1,8 +0,0 @@
-import { SurveyType } from '@types'
-import axios from 'axios'
-
-export const getSurvey = async (): Promise<SurveyType> => {
-  return axios
-    .get('http://localhost:8787' + '/survey') // TODO: replace with BASE_URL
-    .then((response) => response.data)
-}
